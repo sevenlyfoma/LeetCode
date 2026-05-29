b91e74e383b0b97f07c5d0526ee479e3dc0b05ca
7lyfoma <7lyfoma@gmail.com>
2026-05-29 12:30:32 +0100

    Add solution and test cases for merge strings alternatively problem

--- merge-strings-alternatively/src/main/java/io/sevenlyfoma/github/App.java
+++ merge-strings-alternatively/src/main/java/io/sevenlyfoma/github/App.java
@@ -1,17 +1,40 @@
 package io.sevenlyfoma.github;
 
 /**
- * Hello world!
+ * Code for merge strings alternatively problem.
  */
 public final class App {
-    private App() {
+    App() {
     }
 
     /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
+     * Merges two words together by adding letters in alternative order.
+     * Remaining letters are added at end in case of unequal word size.
+     * @param word1 first string input where 1 <= word1.length() <= 100
+     * @param word2 second string input where 1 <= word1.length() <= 100
+     * @return String containing merged words
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public String mergeAlternately(String word1, String word2) {
+        String result = "";
+
+        int lowerLength = word1.length();
+        int higherLength = word2.length();
+        String remainderWord = word2;
+        if (word2.length() < lowerLength) {
+            lowerLength = word2.length();
+            higherLength = word1.length();
+            remainderWord = word1;
+        }
+
+        for (int i = 0; i < lowerLength; i++) {
+            result += word1.charAt(i);
+            result += word2.charAt(i);
+        }
+
+        for (int i = lowerLength; i < higherLength; i++) {
+            result += remainderWord.charAt(i);
+        }
+
+        return result;
     }
 }
--- merge-strings-alternatively/src/test/java/io/sevenlyfoma/github/AppTest.java
+++ merge-strings-alternatively/src/test/java/io/sevenlyfoma/github/AppTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple Merge Strings Alternatively problem.
  */
 class AppTest {
     /**
@@ -15,4 +15,42 @@ class AppTest {
     void testApp() {
         assertEquals(1, 1);
     }
+
+    /**
+     * Test basic case of two 3 letter inputs being merged.
+     */
+    @Test
+    void testEqualLength() {
+        App a = new App();
+        assertEquals("axbycz", a.mergeAlternately("abc", "xyz"));
+    }
+
+    /**
+     * Test basic case of the same inputs as "testEqualLength" but the input order has changed.
+     */
+    @Test
+    void testInputOrderMatters() {
+        App a = new App();
+        assertEquals("xaybzc", a.mergeAlternately("xyz", "abc"));
+    }
+
+
+    /**
+     * Test case of the inputs being differnent length, with the first word longer.
+     */
+    @Test
+    void testUnequalLengthWord1Longer() {
+        App a = new App();
+        assertEquals("axbyczddd", a.mergeAlternately("abcddd", "xyz"));
+    }
+
+    /**
+     * Test case of the inputs being differnent length, with the second word longer.
+     */
+    @Test
+    void testUnequalLengthWord2Longer() {
+        App a = new App();
+        assertEquals("axbyczwww", a.mergeAlternately("abc", "xyzwww"));
+    }
+
 }
