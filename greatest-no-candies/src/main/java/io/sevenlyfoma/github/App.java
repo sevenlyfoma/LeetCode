75c4aa9ce4cddfc10002f28ecee3d1721e68c19c
7lyfoma <7lyfoma@gmail.com>
2026-05-30 15:20:50 +0100

    Added mvn project for "Kids With the Greatest Number of Candies" problem

---
+++ greatest-no-candies/src/main/java/io/sevenlyfoma/github/App.java
@@ -0,0 +1,17 @@
+package io.sevenlyfoma.github;
+
+/**
+ * Solution for problem "Kids With the Greatest Number of Candies".
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Test.
+     * @return int
+     */
+    public static int test() {
+        return 0;
+    }
+}
