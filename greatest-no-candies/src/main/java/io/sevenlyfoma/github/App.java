f5adf19098f8c4f12526676754f8e8ad78a21b78
7lyfoma <7lyfoma@gmail.com>
2026-05-31 10:47:46 +0100

    added solution for "kids with the greatest number of candies"

--- greatest-no-candies/src/main/java/io/sevenlyfoma/github/App.java
+++ greatest-no-candies/src/main/java/io/sevenlyfoma/github/App.java
@@ -1,5 +1,8 @@
 package io.sevenlyfoma.github;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Solution for problem "Kids With the Greatest Number of Candies".
  */
@@ -13,5 +16,40 @@ public final class App {
      */
     public static int test() {
         return 0;
+    }
+
+    /**
+     * Solution code.
+     * @param candies
+     * @param extraCandies
+     * @return List<Boolean>
+     */
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = findMaxCandies(candies);
+
+        Boolean[] extraChecked = new Boolean[candies.length];
+
+        for (int i = 0; i < candies.length; i++) {
+            extraChecked[i] = candies[i] + extraCandies >= max;
+        }
+
+        return Arrays.asList(extraChecked);
+    }
+
+    /**
+     * Helper function.
+     * @param candies
+     * @return int
+     */
+    public static int findMaxCandies(int[] candies) {
+        int max = 0;
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+
+        return max;
     }
 }
--- greatest-no-candies/src/test/java/io/sevenlyfoma/github/AppTest.java
+++ greatest-no-candies/src/test/java/io/sevenlyfoma/github/AppTest.java
@@ -1,5 +1,8 @@
 package io.sevenlyfoma.github;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +17,29 @@ class AppTest {
     @Test
     void testApp() {
         assertEquals(0, App.test());
+    }
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testMostlyTrue() {
+        List<Boolean> res = Arrays.asList(true, true, false, true, true);
+        int[] inp = {5, 4, 1, 2, 3};
+        assertEquals(res, App.kidsWithCandies(inp, 3));
+    }
+
+    @Test
+    void testMostlyFalse() {
+        List<Boolean> res = Arrays.asList(true, false, false, false, false);
+        int[] inp = {5, 4, 1, 2, 3};
+        assertEquals(res, App.kidsWithCandies(inp, 0));
+    }
+
+    @Test
+    void testAllTrue() {
+        List<Boolean> res = Arrays.asList(true, true, true);
+        int[] inp = {5, 4, 1};
+        assertEquals(res, App.kidsWithCandies(inp, 200));
     }
 }
