6802ae454ad4004716b39bfaf071e1a00472a54f
7lyfoma <7lyfoma@gmail.com>
2026-06-01 15:43:31 +0100

    added code and tests for "Can Place Flowers"

--- can-place-flowers/src/main/java/io/sevenlyfoma/github/App.java
+++ can-place-flowers/src/main/java/io/sevenlyfoma/github/App.java
@@ -10,4 +10,43 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        //We want to count the number of stretchs of zeros and their lengths
+        //A stretch of 3 zeros means 1 placeable, 5 means 2, 7 means 3 etc
+        //2 3s mean 2 etc. 
+        // We can probably do this in one iteration
+
+        int currentStretchLength = 1;
+
+        int remainingFlowersToPlace = n;
+
+        int i = 0;
+        while (i < flowerbed.length && remainingFlowersToPlace != 0) {
+
+            if (flowerbed[i] == 1){
+                currentStretchLength = 0;
+            }
+
+            else{
+                currentStretchLength += 1;
+
+                if (currentStretchLength % 2 == 1 && currentStretchLength != 1){
+                    remainingFlowersToPlace -= 1;
+                    currentStretchLength = 1;
+                }
+            }
+
+            i++;
+
+        }
+
+        if (currentStretchLength == 2 && remainingFlowersToPlace == 1){
+            remainingFlowersToPlace = 0;
+        }
+
+
+
+        return (remainingFlowersToPlace == 0);
+    }
 }
--- can-place-flowers/src/test/java/io/sevenlyfoma/github/AppTest.java
+++ can-place-flowers/src/test/java/io/sevenlyfoma/github/AppTest.java
@@ -1,5 +1,6 @@
 package io.sevenlyfoma.github;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,84 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+
+    @Test
+    public void threeSpacesOnePlace()
+    {
+        int[] inp = {1, 0, 0, 0, 1};
+        assertEquals(true, App.canPlaceFlowers(inp, 1));
+    }
+
+    @Test
+    public void threeSpacesTwoPlace()
+    {
+        int[] inp = {1, 0, 0, 0, 1};
+        assertEquals(false, App.canPlaceFlowers(inp, 2));
+    }
+
+    @Test
+    public void threeSpacesZeroPlace()
+    {
+        int[] inp = {1, 0, 0, 0, 1};
+        assertEquals(true, App.canPlaceFlowers(inp, 0));
+    }
+
+    @Test
+    public void fiveSpacesOnePlace()
+    {
+        int[] inp = {1, 0, 0, 0, 0, 0, 1};
+        assertEquals(true, App.canPlaceFlowers(inp, 1));
+    }
+
+    @Test
+    public void fiveSpacesTwoPlace()
+    {
+        int[] inp = {1, 0, 0, 0, 0, 0, 1};
+        assertEquals(true, App.canPlaceFlowers(inp, 2));
+    }
+
+    @Test
+    public void fiveSpacesThrePlace()
+    {
+        int[] inp = {1, 0, 0, 0, 0, 0, 1};
+        assertEquals(false, App.canPlaceFlowers(inp, 3));
+    }
+
+    @Test
+    public void twothreeSpacesTwoPlace()
+    {
+        int[] inp = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+        assertEquals(true, App.canPlaceFlowers(inp, 2));
+    }
+
+    
+    @Test
+    public void twothreeSpacesThreePlace()
+    {
+        int[] inp = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+        assertEquals(false, App.canPlaceFlowers(inp, 3));
+    }
+
+    @Test
+    public void startEmpty()
+    {
+        int[] inp = {0, 0, 1};
+        assertEquals(true, App.canPlaceFlowers(inp, 1));
+    }
+
+    @Test
+    public void endEmpty()
+    {
+        int[] inp = {1, 0, 0};
+        assertEquals(true, App.canPlaceFlowers(inp, 1));
+    }
+
+    @Test
+    public void endEmptyLong()
+    {
+        int[] inp = {1, 0, 0, 0, 0};
+        assertEquals(true, App.canPlaceFlowers(inp, 2));
     }
 }
