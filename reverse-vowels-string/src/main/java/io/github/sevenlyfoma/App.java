f55913d3a11d23b39e81de8fec5450156c30632e
7lyfoma <7lyfoma@gmail.com>
2026-06-02 19:43:31 +0100

    Added solutiopn for "reverse vowels of a string"

--- reverse-vowels-string/src/main/java/io/github/sevenlyfoma/App.java
+++ reverse-vowels-string/src/main/java/io/github/sevenlyfoma/App.java
@@ -1,13 +1,74 @@
 package io.github.sevenlyfoma;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+
+    public static void main(String[] args){
+        reverseVowels("IceCreAm");
+    }
+
+    public static String reverseVowels(String s) {
+        
+
+        String lowercase = s.toLowerCase();
+
+        ArrayList<Integer> positions = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++){
+            char c = lowercase.charAt(i);
+            
+
+            if (isVowel(c)){
+                positions.add(i);
+            }
+        
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int currentReplacer = positions.size()-1;
+
+        for (int i = 0; i < s.length(); i++){
+            char c = lowercase.charAt(i);
+
+            if (isVowel(c)){
+                positions.add(i);
+                char cx = s.charAt(positions.get(currentReplacer));
+                currentReplacer -= 1;
+                sb.append(cx);
+            }
+
+            else{
+                char cx = s.charAt(i);
+                sb.append(cx);
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+
+    public static Boolean isVowel(char c){
+        
+        Boolean isVowel = false;
+        int j = 0;
+        while (!isVowel && j < 5) {
+            if (vowels[j] == c){
+                isVowel = true;
+            }
+            j++;
+        }
+
+        return isVowel;
     }
 }
--- reverse-vowels-string/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ reverse-vowels-string/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,5 +1,6 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,22 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void upperLowerTests()
+    {
+        assertEquals("AceCreIm", App.reverseVowels("IceCreAm"));
+    }
+
+
+    @Test
+    public void testIsVowel()
+    {
+        assertEquals(true, App.isVowel('a'));
+        assertEquals(true, App.isVowel('e'));
+        assertEquals(true, App.isVowel('i'));
+        assertEquals(true, App.isVowel('o'));
+        assertEquals(true, App.isVowel('u'));
     }
 }
