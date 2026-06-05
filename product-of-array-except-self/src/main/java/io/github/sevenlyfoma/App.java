8161f47d45f6549ba4a0499bec5da5394389815e
7lyfoma <7lyfoma@gmail.com>
2026-06-05 14:28:47 +0100

    added solution and tests for "Product of Array Except Self"

--- product-of-array-except-self/src/main/java/io/github/sevenlyfoma/App.java
+++ product-of-array-except-self/src/main/java/io/github/sevenlyfoma/App.java
@@ -8,11 +8,62 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldg!" );
+        int[] inp = {1, 2, 3, 0, 0};
+        printArray(productExceptSelf(inp));
+    }
+
+    public static void printArray(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            System.out.print(",");
+        }
+        System.out.print("\n");
     }
 
     public static int[] productExceptSelf(int[] nums) {
-        
-        return null;
+
+        int productWithoutZero = 1;
+
+        int numberOfZeros = 0;
+
+        int l = nums.length;
+
+        for (int i = 0; i < l; i++) {
+            int n = nums[i];
+            if (n != 0){
+                productWithoutZero *= n;
+            }
+            else{
+                numberOfZeros++;
+            }
+        }
+
+        if (numberOfZeros > 1){
+            for (int i = 0; i < l; i++) {
+                nums[i] = 0;
+
+                
+            }
+            return nums;
+        }
+
+        else if (numberOfZeros == 1){
+            for (int i = 0; i < l; i++) {
+                if (nums[i] == 0) {
+                    nums[i] = productWithoutZero;
+                }
+                else {
+                    nums[i] = 0;
+                }
+            }
+            return nums;
+        }
+
+        else {
+            for (int i = 0; i < l; i++) {
+                nums[i] = productWithoutZero / nums[i];
+            }
+            return nums;
+        }
     }
 }
--- product-of-array-except-self/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ product-of-array-except-self/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,5 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +19,45 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void allZeros()
+    {
+        int[] exp = {0, 0, 0, 0, 0};
+
+        int[] inp = {0, 0, 0, 0, 0};
+
+        assertArrayEquals(exp, App.productExceptSelf(inp));
+    }
+
+    @Test
+    public void twoZeros()
+    {
+        int[] exp = {0, 0, 0, 0, 0};
+
+        int[] inp = {1, 2, 3, 0, 0};
+
+        assertArrayEquals(exp, App.productExceptSelf(inp));
+    }
+
+    @Test
+    public void oneZero()
+    {
+        int[] exp = {0, 0, 0, 0, 24};
+
+        int[] inp = {1, 2, 3, 4, 0};
+
+        assertArrayEquals(exp, App.productExceptSelf(inp));
+    }
+
+    @Test
+    public void noZeros()
+    {
+        int[] exp = {120, 60, 40, 30, 24};
+
+        int[] inp = {1, 2, 3, 4, 5};
+
+        assertArrayEquals(exp, App.productExceptSelf(inp));
+    }
+    
 }
