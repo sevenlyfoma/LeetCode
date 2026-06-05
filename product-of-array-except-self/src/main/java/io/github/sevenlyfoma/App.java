d3f7a03d7b81ee2c0750042073f1229f8021d7d4
7lyfoma <7lyfoma@gmail.com>
2026-06-05 13:52:30 +0100

    added mvn project for "Product of Array Except Self"

---
+++ product-of-array-except-self/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,18 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldg!" );
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        
+        return null;
+    }
+}
