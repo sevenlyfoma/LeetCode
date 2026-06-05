9c74188c322ff7c8795f0b7fc36383a61b3451b9
7lyfoma <7lyfoma@gmail.com>
2026-06-05 14:32:06 +0100

    added project for "Increasing Triplet Subsequence"

---
+++ increasing-triplet-subsequence/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,19 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static boolean increasingTriplet(int[] nums) {
+        
+
+        return true;
+    }
+}
