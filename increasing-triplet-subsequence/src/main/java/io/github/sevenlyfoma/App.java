6b43db7fc87a87f3360a78be6d86532f80fc27ff
7lyfoma <7lyfoma@gmail.com>
2026-06-06 11:18:00 +0100

    Added code and tests for "increasing-triplet-subsequence"

--- increasing-triplet-subsequence/src/main/java/io/github/sevenlyfoma/App.java
+++ increasing-triplet-subsequence/src/main/java/io/github/sevenlyfoma/App.java
@@ -1,5 +1,7 @@
 package io.github.sevenlyfoma;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -11,9 +13,89 @@ public class App
         System.out.println( "Hello World!" );
     }
 
-    public static boolean increasingTriplet(int[] nums) {
-        
 
-        return true;
+    public static int[] increasingTripletRes(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        int[] result = {0,0,0};
+
+        for (int i = 0; i < nums.length; i++){
+            int x = nums[i];
+            if (x <= min1){
+                min1 = x;
+            }
+            else if (x <= min2){
+                min2 = x;
+                result[0] = min1;
+                result[1]= min2;
+            }
+            else{
+                result[2] = x;
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean increasingTriplet(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++){
+            int x = nums[i];
+            if (x <= min1){
+                min1 = x;
+            }
+            else if (x <= min2){
+                min2 = x;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    //Uses a O(n^3) algorithm, too slow to pass test cases
+    public static boolean increasingTripletCubic(int[] nums) {
+        if (nums.length < 3){
+            return false;
+        }
+
+        boolean notFound = true;
+
+        int i = 0;
+        while (i < nums.length - 2 && notFound){
+            int j = i + 1;
+
+            while (j < nums.length - 1 && notFound){
+
+                if (nums[j] > nums[i]){
+                    int k = j + 1;
+
+                    while (k < nums.length && notFound){
+                        if (nums[k] > nums[j]){
+                            notFound = false;
+                        }
+                        k++;
+
+                    }
+
+
+                }
+
+                j++;
+
+
+            }
+            i++;
+
+        }
+
+        return !notFound;
     }
 }
