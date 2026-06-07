7471d3e03f3e4a0e2408aa5cee454d9d04c52420
7lyfoma <7lyfoma@gmail.com>
2026-06-07 18:37:00 +0100

    Added solution for "move zeroes"

--- move-zeroes/src/main/java/io/github/sevenlyfoma/App.java
+++ move-zeroes/src/main/java/io/github/sevenlyfoma/App.java
@@ -13,5 +13,24 @@ public class App
 
     public static void moveZeroes(int[] nums) {
         
+        int currentNumberOfZeroes = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            int x = nums[i];
+
+            if (x == 0){
+                currentNumberOfZeroes += 1;
+            }
+            else {
+                if (currentNumberOfZeroes > 0){
+                    nums[i-currentNumberOfZeroes] = nums[i];
+                }
+            }
+
+        }
+
+        for (int i = nums.length - currentNumberOfZeroes; i < nums.length; i ++){
+            nums[i] = 0;
+        }
     }
 }
--- move-zeroes/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ move-zeroes/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,5 +1,6 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,27 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void test1()
+    {
+        int[] inp = {0,1,0,3,12};
+        int[] exp = {1,3,12,0,0};
+
+        App.moveZeroes(inp);
+
+        assertArrayEquals(exp, inp);
+    }
+
+    @Test
+    public void testOneZero()
+    {
+        int[] inp = {0};
+        int[] exp = {0};
+
+        App.moveZeroes(inp);
+
+        assertArrayEquals(exp, inp);
     }
 }
