0c15bee5e1e73b18d674748dad4b174b9d4e94a9
7lyfoma <7lyfoma@gmail.com>
2026-06-09 15:11:14 +0100

    added project for "Max Number of K-Sum Pairs"

---
+++ max-num-k-sum-pairs/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,19 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int maxOperations(int[] nums, int k) {
+        
+
+        return 0;
+    }
+}
