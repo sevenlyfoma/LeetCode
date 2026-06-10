5cc1f87bb94f1bdae12b653cfa9b936798d712a2
7lyfoma <7lyfoma@gmail.com>
2026-06-10 17:06:53 +0100

    added project for "Max Consecutive Ones III"

---
+++ max-consecutive-ones-iii/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,17 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int longestOnes(int[] nums, int k) {
+        return 0;
+    }
+}
