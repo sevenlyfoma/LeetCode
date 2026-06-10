5192332f25801037834d49653ec7c0c21e5439f3
7lyfoma <7lyfoma@gmail.com>
2026-06-10 11:30:12 +0100

    added project for "Maximum Average Subarray I"

---
+++ max-average-subarray-i/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,18 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static double findMaxAverage(int[] nums, int k) {
+        
+        return 0d;
+    }
+}
