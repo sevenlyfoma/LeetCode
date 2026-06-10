009acc44031eb4567f388f28ce89c3196d580bb1
7lyfoma <7lyfoma@gmail.com>
2026-06-10 13:56:40 +0100

    Added solution for "Maximum Average Subarray I"

--- max-average-subarray-i/src/main/java/io/github/sevenlyfoma/App.java
+++ max-average-subarray-i/src/main/java/io/github/sevenlyfoma/App.java
@@ -8,11 +8,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int[] nums = {-1};
+        System.out.println(findMaxAverage(nums, 1));
     }
 
     public static double findMaxAverage(int[] nums, int k) {
+        int maxTotal = Integer.MIN_VALUE;
         
-        return 0d;
+        int runningTotal = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            runningTotal += nums[i];
+            if (i >= k){
+                runningTotal -= nums[i-k];
+            }
+
+            if (i >= k-1){
+                if (runningTotal > maxTotal){
+                    maxTotal = runningTotal;
+                }
+            }
+        }
+
+        return (double) maxTotal / (double) k;
     }
 }
--- max-average-subarray-i/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ max-average-subarray-i/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -16,5 +16,19 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void sampleTest1()
+    {
+        int[] nums = {1,12,-5,-6,50,3};
+        assertEquals(12.75d, App.findMaxAverage(nums, 4), 0.00001);
+    }
+
+    @Test
+    public void sampleTest2()
+    {
+        int[] nums = {-1};
+        assertEquals(-1, App.findMaxAverage(nums, 1), 0.00001);
     }
 }
