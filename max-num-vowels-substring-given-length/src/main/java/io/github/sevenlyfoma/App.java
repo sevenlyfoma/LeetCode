a400aa9e0a2fef63fba58a73f7193c058af36514
7lyfoma <7lyfoma@gmail.com>
2026-06-10 13:59:22 +0100

    created project for "Maximum Number of Vowels in a Substring of Given Length"

---
+++ max-num-vowels-substring-given-length/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,18 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int maxVowels(String s, int k) {
+        
+        return 0;
+    }
+}
