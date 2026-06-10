c2176baec9ae848e72aa9450132fda505eb6ba35
7lyfoma <7lyfoma@gmail.com>
2026-06-10 17:04:13 +0100

    added solution for "Maximum Number of Vowels in a Substring of Given Length"

--- max-num-vowels-substring-given-length/src/main/java/io/github/sevenlyfoma/App.java
+++ max-num-vowels-substring-given-length/src/main/java/io/github/sevenlyfoma/App.java
@@ -8,15 +8,40 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(maxVowels("ramadan", 2));
     }
     
     public static int maxVowels(String s, int k) {
         
         //Use sliding window approach
-        //
 
+        int maxNum = 0;
+        int runningNum = 0;
 
-        return 0;
+        for (int i=0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if (isVowel(c)){
+                runningNum++;
+            }
+
+            if (i>=k){
+                char ci = s.charAt(i-k);
+                if (isVowel(ci)){
+                    runningNum--;
+                }
+            }
+
+            if (runningNum > maxNum){
+                maxNum = runningNum;
+            }
+                
+        }
+
+        return maxNum;
+    }
+
+    public static boolean isVowel(char c){
+        return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
     }
 }
--- max-num-vowels-substring-given-length/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ max-num-vowels-substring-given-length/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -17,4 +17,43 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void endOfWindowTest()
+    {
+        assertEquals(1, App.maxVowels("bbbbbi", 2));
+    }
+
+    @Test
+    public void fullLengthWindowTest()
+    {
+        assertEquals(3, App.maxVowels("biiib", 3));
+    }
+
+    @Test
+    public void moreThanLengthWindowTest()
+    {
+        assertEquals(3, App.maxVowels("biiiib", 3));
+    }
+
+    @Test
+    public void lessThanLengthWindowTest()
+    {
+        assertEquals(2, App.maxVowels("biib", 3));
+    }
+
+    @Test
+    public void oneManyNonConsecutiveTest()
+    {
+        assertEquals(1, App.maxVowels("ramadan", 2));
+    }
+
+    @Test
+    public void nonConsecutiveBigWindow()
+    {
+        assertEquals(2, App.maxVowels("ramadan", 3));
+    }
+
+    
+
 }
