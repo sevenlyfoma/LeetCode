6e87bc3f6a8817301b8de89df2b12224721b0f5b
7lyfoma <7lyfoma@gmail.com>
2026-06-11 14:05:45 +0100

    Added very slow solution for "Max Consecutive Ones III"

--- max-consecutive-ones-iii/src/main/java/io/github/sevenlyfoma/App.java
+++ max-consecutive-ones-iii/src/main/java/io/github/sevenlyfoma/App.java
@@ -12,6 +12,87 @@ public class App
     }
 
     public static int longestOnes(int[] nums, int k) {
-        return 0;
+        
+
+        int runningLongest = 0;
+        int maxLongest = 0;
+
+        int flippedZeroes = 0;
+
+        int previousZeroPosition = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int x = nums[i];
+
+            if (x==1){
+                runningLongest++;
+            }
+            else{
+                if (previousZeroPosition == -1){
+                    nums[i] = -1;
+                    previousZeroPosition = i;
+                }
+                else{
+                    nums[i] = previousZeroPosition;
+                    previousZeroPosition = i;
+                }
+
+                if (flippedZeroes < k){
+                    flippedZeroes ++;
+                    runningLongest ++;
+                }
+                else {
+                    // runningLongest ++;
+
+                    int totalDistanceToFirstZero = 0;
+                    boolean firsttZeroFound = false;
+
+                    int currentZeroPosition = previousZeroPosition;
+                    int secondToLastZeroPosition = -1;
+
+                    int count = 0;
+
+                    while (!firsttZeroFound && count <= k){
+                        int currentZero = nums[currentZeroPosition];
+
+                        if (currentZero == -1){
+                            totalDistanceToFirstZero = i - currentZeroPosition;
+                            firsttZeroFound = true;
+                            if (secondToLastZeroPosition != -1){
+                                nums[secondToLastZeroPosition]= -1;
+                            }
+                            
+                        }
+                        else{
+                            secondToLastZeroPosition = currentZeroPosition;
+                            currentZeroPosition = currentZero;
+                        }
+                        count++;
+
+                    }
+
+                    runningLongest = totalDistanceToFirstZero;
+
+
+
+                }
+            }
+
+
+
+            if (runningLongest > maxLongest){
+                maxLongest = runningLongest;
+            }
+        }
+
+
+
+        if (runningLongest > maxLongest){
+            maxLongest = runningLongest;
+        }
+
+
+        return maxLongest;
     }
 }
--- max-consecutive-ones-iii/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ max-consecutive-ones-iii/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -16,5 +16,47 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void allOnes()
+    {
+        int[] inp = {1,1,1,1};
+        assertEquals(4, App.longestOnes(inp, 2));
+    }
+
+    @Test
+    public void oneZeroOneK()
+    {
+        int[] inp = {1,1,0,1};
+        assertEquals(4, App.longestOnes(inp, 1));
+    }
+
+    @Test
+    public void oneZeroNoK()
+    {
+        int[] inp = {1,1,0,1};
+        assertEquals(2, App.longestOnes(inp, 0));
+    }
+
+    @Test
+    public void twoZeroNoK()
+    {
+        int[] inp = {1,1,0,1};
+        assertEquals(2, App.longestOnes(inp, 0));
+    }
+
+    @Test
+    public void sampleTest1()
+    {
+        int[] inp = {1,1,1,0,0,0,1,1,1,1,0};
+        assertEquals(6, App.longestOnes(inp, 2));
+    }
+
+    @Test
+    public void sampleTest2()
+    {
+        int[] inp = {0,0,1,1,1,0,0};
+        assertEquals(3, App.longestOnes(inp, 0));
     }
 }
