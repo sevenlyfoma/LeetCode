0e143c20e8c33956ff40eead2dd4f67bf2eebc3c
7lyfoma <7lyfoma@gmail.com>
2026-06-12 12:32:00 +0100

    Added solution for "Longest Subarray of 1's After Deleting One Element"

--- longest-subarray-of-ones-after-deleting-one-element/src/main/java/io/github/sevenlyfoma/App.java
+++ longest-subarray-of-ones-after-deleting-one-element/src/main/java/io/github/sevenlyfoma/App.java
@@ -12,6 +12,45 @@ public class App
     }
 
     public static int longestSubarray(int[] nums) {
-        return 0;
+
+        int maxCount = 0;
+        int runningCount = 0;
+
+        int currentDeletion = -1;
+
+        for (int i = 0; i < nums.length; i++){
+            int x = nums[i];
+
+            if (x == 1){
+                runningCount ++;
+            }
+
+            else{
+
+                
+                if (currentDeletion != -1){
+                    runningCount = i - currentDeletion - 1;
+                }
+
+                currentDeletion = i;
+
+
+            }
+
+
+
+
+            if (runningCount > maxCount){
+                maxCount = runningCount;
+            }
+        }
+
+        if (currentDeletion == -1){
+            maxCount--;
+        }
+
+
+
+        return maxCount;
     }
 }
--- longest-subarray-of-ones-after-deleting-one-element/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ longest-subarray-of-ones-after-deleting-one-element/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -16,5 +16,57 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+
+    @Test
+    public void allOnes()
+    {
+        int[] inp = {1,1,1};
+        assertEquals(2, App.longestSubarray(inp));
+    }
+
+    @Test
+    public void oneZeroNonBlocking()
+    {
+        int[] inp = {1,1,1,0};
+        assertEquals(3, App.longestSubarray(inp));
+    }
+
+    @Test
+    public void oneZeroBlocking()
+    {
+        int[] inp = {1,1,0,1};
+        assertEquals(3, App.longestSubarray(inp));
+    }
+
+    @Test
+    public void twoZeroTrueBlocking()
+    {
+        int[] inp = {1,1,0,0,1};
+        assertEquals(2, App.longestSubarray(inp));
+    }
+
+    @Test
+    public void gapTest()
+    {
+        int[] inp = {1,1,0,0,1,1,1,1};
+        assertEquals(4, App.longestSubarray(inp));
+    }
+
+    
+
+    @Test
+    public void middleBig()
+    {
+        int[] inp = {0,1,1,1,0,1,1,0,1};
+        assertEquals(5, App.longestSubarray(inp));
+    }
+
+    @Test
+    public void allZeros()
+    {
+        int[] inp = {0,0,0,0,0};
+        assertEquals(0, App.longestSubarray(inp));
     }
 }
