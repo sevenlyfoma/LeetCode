232d7dd04b98a600cc0aec535819e3efd31023c7
7lyfoma <7lyfoma@gmail.com>
2026-06-13 09:51:10 +0100

    changed java version for all old projects, added project for "asteroid collision"

---
+++ asteroid-collision/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,19 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int[] asteroidCollision(int[] asteroids) {
+        
+
+        return null;
+    }
+}
