88c010b6c89ebd92d4e428e33ca79065808d30f0
7lyfoma <7lyfoma@gmail.com>
2026-06-13 10:50:59 +0100

    added solution for "Asteroid Collision"

--- asteroid-collision/src/main/java/io/github/sevenlyfoma/App.java
+++ asteroid-collision/src/main/java/io/github/sevenlyfoma/App.java
@@ -1,4 +1,5 @@
 package io.github.sevenlyfoma;
+import java.util.Arrays;
 
 /**
  * Hello world!
@@ -8,12 +9,74 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int[] inp = {-2,-2,1,-2};
+        System.out.println("" + Arrays.toString(asteroidCollision(inp)));
+
     }
 
     public static int[] asteroidCollision(int[] asteroids) {
+        int[] collided = new int[asteroids.length];
         
 
-        return null;
+        int head = -1;
+
+        for (int i = 0; i < asteroids.length; i++){
+            int x = asteroids[i];
+
+            // System.out.println("" + Arrays.toString(collided));
+            // System.out.println("" + head);
+
+
+            if (head == -1){
+                collided[0] = x;
+                head++;
+            }
+            else{
+                int currentHead = collided[head];
+
+                boolean notDestroyed = true;
+            
+                while ((x < 0 && currentHead > 0 && head != -1 && notDestroyed)){
+                    
+                    int absX = Math.abs(x);
+                    int absCH = Math.abs(currentHead);
+
+                    // System.out.println("" + absX + " " + absCH);
+
+                    if (absX == absCH){
+                        // System.out.println("e0");
+                        head --;
+                        notDestroyed = false;
+                    }
+                    else if (absX > absCH){
+                        // System.out.println("e1");
+                        head --;
+                    }
+                    else{
+                        // System.out.println("e2");
+                        notDestroyed = false;
+                    }
+                    
+
+                    if (head != -1){
+                        currentHead = collided[head];
+                    }
+                }
+
+                if (notDestroyed){
+                    head++;
+                    collided[head] = x;
+                    // System.out.println("" + Arrays.toString(collided));
+                }
+            }
+
+            
+
+
+        }
+
+        
+
+        return Arrays.copyOfRange(collided, 0, head+1);
     }
 }
