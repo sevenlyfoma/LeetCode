24e36b751a60f07e32b3e106d9bcf5c81c953611
7lyfoma <7lyfoma@gmail.com>
2026-06-13 06:20:45 +0100

    added project for "find pivot index"

---
+++ find-pivot-index/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,20 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int pivotIndex(int[] nums) {
+        
+
+
+        return 0;
+    }
+}
