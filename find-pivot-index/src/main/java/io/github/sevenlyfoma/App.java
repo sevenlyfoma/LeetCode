e29d98a3720f40c55cc6b368304e72e9c66c684f
7lyfoma <7lyfoma@gmail.com>
2026-06-13 06:39:14 +0100

    added solution for "Find Pivot Index"

--- find-pivot-index/src/main/java/io/github/sevenlyfoma/App.java
+++ find-pivot-index/src/main/java/io/github/sevenlyfoma/App.java
@@ -10,11 +10,49 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public static int pivotIndex(int[] nums){
+        int sum = 0;
+        for (int i = 0; i < nums.length; i ++){
+            sum += nums[i];
+        }
 
-    public static int pivotIndex(int[] nums) {
+        int halfSum = 0;
+        for (int i = 0; i < nums.length; i ++){
+            int target = sum - nums[i];
+            if (target % 2 == 0 && halfSum == (target/2)){
+                return i;
+            }
+            halfSum += nums[i];
+        }
+
+
+
+        return -1;
+    }
+
+    public static int pivotIndexNonNegative(int[] nums) {
         
+        int lp = 0;
+        int ls = 0;
+        int rp = nums.length-1;
+        int rs = 0;
+
+        while (lp != rp){
+            if (rs <= ls){
+                rs += nums[rp];
+                rp--;
+            }
+            else{
+                ls += nums[lp];
+                lp++;
+            }
+        }
+
+        if (ls == rs){
+            return lp;
+        }
 
 
-        return 0;
+        return -1;
     }
 }
--- find-pivot-index/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ find-pivot-index/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -16,5 +16,26 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void sampleTest1()
+    {
+        int[] inp = {1,7,3,6,5,6};
+        assertEquals(3, App.pivotIndex(inp));
+    }
+
+    @Test
+    public void multiplePivots()
+    {
+        int[] inp = {0,0,0,0,0,0};
+        assertEquals(0, App.pivotIndex(inp));
+    }
+
+    @Test
+    public void manyNegatives()
+    {
+        int[] inp = {-1,-1,-1,-1,-1,0};
+        assertEquals(2, App.pivotIndex(inp));
     }
 }
