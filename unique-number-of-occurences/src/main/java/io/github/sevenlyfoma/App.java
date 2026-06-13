5760095706b8ffcdbe3f46eaa98052c2d2e6b8cc
7lyfoma <7lyfoma@gmail.com>
2026-06-13 07:44:20 +0100

    added solution for "unique-number-of-occurences"

--- unique-number-of-occurences/src/main/java/io/github/sevenlyfoma/App.java
+++ unique-number-of-occurences/src/main/java/io/github/sevenlyfoma/App.java
@@ -1,5 +1,7 @@
 package io.github.sevenlyfoma;
 
+import java.util.HashMap;
+
 /**
  * Hello world!
  *
@@ -12,7 +14,26 @@ public class App
     }
 
     public static boolean uniqueOccurrences(int[] arr) {
+
+        HashMap<Integer, Integer> occurenceCount = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++){
+            int x = arr[i];
+            occurenceCount.putIfAbsent(x, 0);
+            occurenceCount.put(x, occurenceCount.get(x)+1);
+        }
+
+        HashMap<Integer, Integer> occurenceCountCount = new HashMap<>();
+
+        for (Integer v: occurenceCount.values()){
+            if (occurenceCountCount.containsKey(v)){
+                return false;
+            }
+            else{
+                occurenceCountCount.put(v, 0);
+            }
+        }
         
-        return false;
+        return true;
     }
 }
--- unique-number-of-occurences/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ unique-number-of-occurences/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -16,5 +16,26 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void sampleTest1()
+    {
+        int[] inp = {1,2,2,1,1,3};
+        assertEquals(true, App.uniqueOccurrences(inp));
+    }
+    
+    @Test
+    public void sampleTest2()
+    {
+        int[] inp = {1,2};
+        assertEquals(false, App.uniqueOccurrences(inp));
+    }
+
+    @Test
+    public void sampleTest3()
+    {
+        int[] inp = {-3,0,1,-3,1,1,1,-3,10,0};
+        assertEquals(true, App.uniqueOccurrences(inp));
     }
 }
