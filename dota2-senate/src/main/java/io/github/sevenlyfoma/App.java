7aad941189bdfb3b549a3453a19e7492c2d857a9
7lyfoma <7lyfoma@gmail.com>
2026-06-14 06:30:12 +0100

    added solution for "dota2-senate"

--- dota2-senate/src/main/java/io/github/sevenlyfoma/App.java
+++ dota2-senate/src/main/java/io/github/sevenlyfoma/App.java
@@ -1,5 +1,7 @@
 package io.github.sevenlyfoma;
 
+import java.util.ArrayDeque;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,78 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static String predictPartyVictory(String senate) {
+
+        int rBans = 0;
+        int dBans = 0;
+
+        char c;
+
+        int rRemainging = 0;
+        int dRemaining = 0;
+
+        ArrayDeque<Character> nextVoteQueue = new ArrayDeque<>();
+
+
+        for (int i = 0; i < senate.length(); i++){
+            c = senate.charAt(i);
+
+
+            if (c == 'R'){
+                if (rBans == 0){
+                    rRemainging++;
+                    dBans++;
+                    nextVoteQueue.add(c);
+                }
+                else{
+                    rBans--;
+                }
+            }
+            else {
+                if (dBans == 0){
+                    dRemaining++;
+                    rBans ++;
+                    nextVoteQueue.add(c);
+                }
+                else{
+                    dBans--;
+                }
+            }
+        }
+
+
+        while (rRemainging > 0 && dRemaining > 0){
+            c = nextVoteQueue.pollFirst();
+
+            if (c == 'R'){
+                if (rBans == 0){
+                    dBans++;
+                    nextVoteQueue.add(c);
+                }
+                else{
+                    rRemainging--;
+                    rBans--;
+                }
+            }
+            else {
+                if (dBans == 0){
+                    rBans ++;
+                    nextVoteQueue.add(c);
+                }
+                else{
+                    dRemaining--;
+                    dBans--;
+                }
+            }
+        }
+
+        if (rRemainging == 0){
+            return "Dire";
+        }
+        else {
+            return "Radiant";
+        }
     }
 }
--- dota2-senate/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ dota2-senate/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.sevenlyfoma;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -16,5 +16,29 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void sampleTest1()
+    {
+        assertEquals("Radiant", App.predictPartyVictory("RD"));
+    }
+
+    @Test
+    public void sampleTest2()
+    {
+        assertEquals("Dire", App.predictPartyVictory("RDD"));
+    }
+
+    @Test
+    public void lateWinners()
+    {
+        assertEquals("Dire", App.predictPartyVictory("RRRRRRDDDDDDDDDDD"));
+    }
+
+     @Test
+    public void sampleTest4()
+    {
+        assertEquals("Dire", App.predictPartyVictory("DDRRR"));
     }
 }
