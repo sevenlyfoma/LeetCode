c87966afad3799a33da5c9e0f63d130e5ea8de6c
7lyfoma <7lyfoma@gmail.com>
2026-06-16 12:56:50 +0100

    added project for "Evaluate Division"

---
+++ evaluate-division/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,20 @@
+package io.github.sevenlyfoma;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        
+        return null;
+    }
+}
