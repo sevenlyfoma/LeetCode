85f717213ca317682bf54d0c737669404792c1d5
7lyfoma <7lyfoma@gmail.com>
2026-06-18 09:39:57 +0100

    added project and solution for "Find Peak Element"

---
+++ find-peak-element/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -0,0 +1,20 @@
+package io.github.sevenlyfoma;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void sampleTest1()
+    {
+        int[] nums = {1,2,3,1};
+        assertEquals(2, App.findPeakElement(nums));
+    }
+}
