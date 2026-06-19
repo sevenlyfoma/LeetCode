2f21218c19fffe0c7e6e7b330ae76e79545bc13d
7lyfoma <7lyfoma@gmail.com>
2026-06-19 08:34:51 +0100

    added project and solution for "Domino and Tromino Tiling"

---
+++ domino-and-tromino-tiling/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -0,0 +1,19 @@
+package io.github.sevenlyfoma;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void sampleTest1()
+    {
+        assertEquals(11, App.numTilings(4));
+    }
+}
