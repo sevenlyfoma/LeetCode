5200712ba77675f2453410cebeeca7d88d537932
7lyfoma <7lyfoma@gmail.com>
2026-06-21 10:10:16 +0100

    added project for "climbing stairs"

---
+++ climbing-stairs/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,17 @@
+package io.github.sevenlyfoma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public int climbStairs(int n) {
+        return 0;
+    }
+}
