8b39f5b4f1cc966866f5223c41b8a4929b5d1531
7lyfoma <7lyfoma@gmail.com>
2026-06-23 13:56:25 +0100

    added project for "Pascal's Triangle"

---
+++ pascals-triangle/src/main/java/io/github/sevenlyfoma/App.java
@@ -0,0 +1,19 @@
+package io.github.sevenlyfoma;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        return null;
+    }
+}
