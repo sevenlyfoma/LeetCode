d1ae8b16ee8a90a35d5f29185744035c432a217e
7lyfoma <7lyfoma@gmail.com>
2026-06-26 17:29:20 +0100

    Added solution for "merge sorted arrays"

--- merge-sorted-array/src/main/java/io/github/sevenlyfoma/App.java
+++ merge-sorted-array/src/main/java/io/github/sevenlyfoma/App.java
@@ -12,10 +12,43 @@ public class App
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int min = Math.min(m, n);
 
-        for (int i = 0; i < min; i ++){
+        int p1 = 0;
+
+        int p2 = 0;
+
+        int i = 0;
+
+        int[] temp = new int[m];
+        
+        System.arraycopy(nums1, 0, temp, 0, m);
+
+        while (p1 != m || p2 != n){
+            if (p1 == m){
+                nums1[i] = nums2[p2];
+                p2++;
+            }
+            else if (p2 == n){
+                nums1[i] = temp[p1];
+                p1++;
+            }
+            else {
+                int x = temp[p1];
+                int y = nums2[p2];
+                if (x <= y){
+                    nums1[i] = x;
+                    p1++;
+                }
+                else{
+                    nums1[i] = y;
+                    p2++;
+                }
+            }
+
             
+
+
+            i++;
         }
     }
 }
