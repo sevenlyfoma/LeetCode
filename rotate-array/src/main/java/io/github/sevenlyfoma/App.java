fda5d8462357d58a647f8cfce5281246499506a8
7lyfoma <7lyfoma@gmail.com>
2026-07-04 17:44:14 +0100

    Added finished solution for "rotate array"

--- rotate-array/src/main/java/io/github/sevenlyfoma/App.java
+++ rotate-array/src/main/java/io/github/sevenlyfoma/App.java
@@ -10,105 +10,65 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] input = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54};
-        int k = 45;
+        // int[] input = {1,2,3,4,5,6,7,8,9,10,11,12};
+        // int k = 7 ;
+
+        int[] input = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27};
+        int k = 38;
 
         App.rotate(input, k);
+
+        System.out.println("" + Arrays.toString(input));
     }
 
     public static void rotate(int[] nums, int k) {
-
-        int rotAmount = k % nums.length;
-
-        int lookForOffSet = 0;
-
-        int looksforChange = rotAmount;
-
-        int lastRotationExit= -1;
-        
-        for (int i = 0; i < nums.length-1; i++){
-
-            int rotationTarget = (i + rotAmount) % nums.length;
-            int replaced = nums[rotationTarget];
-
-            if (i >= looksforChange){
-                looksforChange = (i + rotAmount);
-                lookForOffSet += 1 + (lastRotationExit % rotAmount);
-                System.out.println(lastRotationExit);
-                System.out.println(lookForOffSet);
-            }
-
-            int xPos = (i - lookForOffSet) % nums.length;
-            int x = nums[xPos];
-
-            nums[xPos] = replaced;
-            nums[rotationTarget] = x;
-
-            
-            lastRotationExit = rotationTarget;
-
-            System.out.println("" + Arrays.toString(nums));
-        }
+        rotateSegment(nums, k, 0, nums.length-1);
     }
 
-    public static void rotateOld(int[] nums, int k) {
-        int alreadyRotatedPos = nums.length+1;
+    public static void rotateSegment(int[] nums, int k, int start, int finish) {
 
-        int rotAmount = k % nums.length;
+        if (start >= finish){
+            return;
+        }
+
+        int length = finish-start+1;
+
+        int rotAmount = k % length;
+
+        if (rotAmount == 0){
+            return;
+        }
 
         int count = 0;
 
-        int modifer = 0;
+        int remainder = (length % rotAmount);
 
-        for (int i = 0; i < nums.length; i++){
+        int wholeNumberRotsN = length - remainder ;
 
-            int rotationTarget = (i + rotAmount) % nums.length;
+
+        int i;
+        for (i = 0; i < wholeNumberRotsN; i++){
+            if (count >= rotAmount){
+                count = 0;
+            }
+
+            int rotationTarget = start+ ((i + rotAmount) % length);
             int replaced = nums[rotationTarget];
 
-            if (i >= alreadyRotatedPos){
-                modifer += count;
-                count = 0;
-                System.out.println(modifer);
-            }
-
-            if (count == 0){
-                alreadyRotatedPos = (i + rotAmount);
-                // System.out.println(alreadyRotatedPos);
-            }
-            int x = nums[i - modifer];
+            int x = nums[start+count];
 
             nums[rotationTarget] =x;
-            nums[i - modifer] = replaced;
+            nums[start + count] = replaced;
+
             count++;
-
-            // if (i < alreadyRotatedPos){
-            //     int x = nums[i - modifer];
-
-            //     nums[rotationTarget] =x;
-            //     nums[i] = replaced;
-            //     count++;
-            // }
-
-            // else{
-            //     modifer = count;
-            //     count = 0;
-
-            // }
-
-            
-
-            // if (i >= rotationTa5rget)
-
-            
-
-            
-
-            System.out.println("" + Arrays.toString(nums));
-
-            
 
         }
 
+        int newRotate = rotAmount - remainder;
+        if (rotAmount - remainder != 0){
+            rotateSegment(nums, newRotate , start+rotAmount-remainder, start+rotAmount-1);
+        }
 
     }
+
 }
--- rotate-array/src/test/java/io/github/sevenlyfoma/AppTest.java
+++ rotate-array/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -23,4 +23,58 @@ public class AppTest
 
         assertArrayEquals(expectec, input);
     }
+
+    @Test
+    public void sampleTest2()
+    {
+        int[] input = {-1,-100,3,99};
+        int k = 2;
+
+        App.rotate(input, k);
+
+        int[] expectec = {3,99,-1,-100};
+
+        assertArrayEquals(expectec, input);
+    }
+
+    @Test
+    public void sampleTest3()
+    {
+        int[] input = {1,2};
+        int k = 0;
+
+        App.rotate(input, k);
+
+        int[] expectec = {1,2};
+
+        assertArrayEquals(expectec, input);
+    }
+
+    @Test
+    public void sampleTest4()
+    {
+        int[] input = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54};
+        int k = 45;
+
+        App.rotate(input, k);
+
+        int[] expectec = {10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,1,2,3,4,5,6,7,8,9};
+
+        assertArrayEquals(expectec, input);
+    }
+
+
+    @Test
+    public void sampleTest5()
+    {
+        int[] input = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27};
+        int k = 38;
+
+        App.rotate(input, k);
+
+        int[] expectec = {17,18,19,20,21,22,23,24,25,26,27,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+        assertArrayEquals(expectec, input);
+    }
+
 }
