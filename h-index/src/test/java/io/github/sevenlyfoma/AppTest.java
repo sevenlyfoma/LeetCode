91b7cfebf631351cbe37201333df5a8b5846535f
7lyfoma <7lyfoma@gmail.com>
2026-07-30 17:21:24 +0100

    Added project for "h-index"

---
+++ h-index/src/test/java/io/github/sevenlyfoma/AppTest.java
@@ -0,0 +1,21 @@
+package io.github.sevenlyfoma;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void sampleTest1()
+    {
+        int[] inp = {1,3,1};
+
+        assertEquals(1, App.hIndex(inp));
+    }
+}
